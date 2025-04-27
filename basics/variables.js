e7f70f2c66209aba378ce3153daa5082eb0c6ce4
vaibhav katiyar <csaiml22028@glbitm.ac.in>
2025-04-27 10:31:46 +0000

    variable

---
+++ basics/variables.js
@@ -0,0 +1,9 @@
+const accId="1234"
+let accEmail="abc@gmail.com"
+var accPassword=9283
+    accCity='up'
+let accState;
+
+/*we do not prefer var bcz of issue in scope */
+
+console.table([accId,accEmail,accPassword,accCity,accState]);
