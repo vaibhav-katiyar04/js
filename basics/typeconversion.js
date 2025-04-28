5b01392e51b18f23420e1f412af690d21a74eca6
vaibhav katiyar <csaiml22028@glbitm.ac.in>
2025-04-28 06:43:42 +0000

    typeconversion

---
+++ basics/typeconversion.js
@@ -0,0 +1,11 @@
+let value1=20
+let value2="20yghyjgj"
+console.table([typeof value2,typeof value1])
+
+let numvalue=Number(value2)
+console.log( numvalue)
+
+
+let val=0
+let boolval=Boolean(val)
+console.log(boolval)
